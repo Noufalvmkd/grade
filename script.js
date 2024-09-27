dc99d4e6cc2cf6e996ce9ff60d07042619255377
noufalvmkd <noufalvmkd@gmail.com>
2024-09-27 07:33:26 +0400

    f

---
+++ script.js
@@ -0,0 +1,60 @@
+let input =document.getElementById("input")
+let btn = document.getElementById("btn")
+let result =document.getElementById("result")
+/*A+ (90-100)
+A (80-89)
+B+ (70-79)
+B (60-69)
+C+ (50-59)
+C (40-49)
+D+ (30-39)
+D (20-29)
+E (Below 20)*/
+
+result.textContent="Result Will Show Here"
+
+
+function gradeEval(){
+    
+    let val = input.value;
+   
+    
+
+if(val>=90 && val<=100){
+    result.textContent="PASSED , GRADE= A+"
+}
+else if (val>=80 && val<=89) {
+    result.textContent="PASSED , GRADE= A"
+}
+ 
+else if (val>=70 && val<=79) {
+result.textContent="PASSED , GRADE= B+"
+} 
+else if (val>=60 && val<=69) {
+result.textContent="PASSED , GRADE= B"
+} 
+else if (val>=50 && val<=59) {
+result.textContent="PASSED , GRADE= C+"
+} 
+else if (val>=40 && val<=49) {
+result.textContent="PASSED , GRADE= C"
+} 
+else if (val>=30 && val<=39) {
+result.textContent="PASSED , GRADE= D+"
+} 
+else{
+    result.textContent=" Minimum passing grade: D+ (30 39)"
+ 
+}
+
+val=""
+
+}
+
+console.log(result)
+
+
+
+btn.addEventListener("click",gradeEval);
+
+result.textContent="PASSED , GRADE A+"
